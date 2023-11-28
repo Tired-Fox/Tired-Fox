f68300023181aa2a241ed1345c15c940335fe848
Zachary Boehm <zboehm@menard-inc.com>
2023-11-28 13:19:43 -0600

    Basic asthetic

--- src/lib/components/custom/pre.svelte
+++ src/lib/components/custom/pre.svelte
@@ -16,60 +16,39 @@
     }
 </script>
 
-<div class="custom-code-highlight">
-    <button type="button" on:click={copyText} aria-label="Copy code as text">
-		{#if !copying}
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-5 h-5"
-                out:fade={{ delay: 0, duration: 50 }}
-                in:fade={{ delay: 50, duration: 50 }}
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"
-				/>
-			</svg>
-		{:else}
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-5 h-5"
-                out:fade={{ delay: 0, duration: 50 }}
-                in:fade={{ delay: 50, duration: 50 }}
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-				/>
-			</svg>
-		{/if}
-	</button>
+<div class="my-2 rounded-md overflow-hidden shadow-sm shadow-zinc-800">
+    <div class="bg-[#222222] px-4 py-2 flex items-center justify-between">
+        <span class="text-sm text-zinc-600">{lang ?? ''}</span>
+        <button type="button" on:click={copyText} aria-label="Copy code as text" class="text-zinc-400">
+            <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class={`w-5 h-5${copying ? ' hidden' : ''}`}
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M8.25 7.5V6.108c0-1.135.845-2.098 1.976-2.192.373-.03.748-.057 1.123-.08M15.75 18H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08M15.75 18.75v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5A3.375 3.375 0 006.375 7.5H5.25m11.9-3.664A2.251 2.251 0 0015 2.25h-1.5a2.251 2.251 0 00-2.15 1.586m5.8 0c.065.21.1.433.1.664v.75h-6V4.5c0-.231.035-.454.1-.664M6.75 7.5H4.875c-.621 0-1.125.504-1.125 1.125v12c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V16.5a9 9 0 00-9-9z"
+                    />
+                </svg>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class={`w-5 h-5${!copying ? ' hidden' : ''}`}
+                >
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                </svg>
+        </button>
+    </div>
 	<span bind:this={pre}><slot /></span>
-    {#if lang} 
-        <span class="lang">{lang}</span>
-    {/if}
 </div>
-
-<style>
-    .custom-code-highlight {
-        @apply relative;
-    }
-
-    .custom-code-highlight > button {
-        @apply absolute top-2 right-2 text-zinc-600 hover:text-zinc-400;
-    }
-
-    .custom-code-highlight > .lang {
-        @apply text-sm text-zinc-600 absolute bottom-2 right-2
-    }
-</style>
---
+++ src/routes/footer.svelte
@@ -0,0 +1,32 @@
+<script>
+    import JamGithub from 'virtual:icons/jam/github';
+    import JamRssFeed from 'virtual:icons/jam/rss-feed';
+
+    import { page } from '$app/stores';
+	import { baseUrl } from '$lib/config';
+</script>
+
+<footer class="text-zinc-400 dark:text-zinc-500 px-4 py-2 flex justify-between w-full max-w-[64rem] mx-auto mb-4">
+    <div></div>
+    <small aria-label="Copyright">&copy; 2023 Zachary Boehm. All Rights Reserved</small>
+    <ul class="flex gap-3">
+        <li>
+            <a href="https://github.com/Tired-Fox" aria-label="Github" target="_blank" title="Github ⭷">
+                <JamGithub />
+            </a>
+        </li>
+        {#if $page.url.pathname.startsWith('/blog')}
+        <li>
+            <a href={`${baseUrl}/rss.xml`} aria-label="Blog RSS Feed" target="_blank" title="RSS Feed ⭷">
+                <JamRssFeed />
+            </a>
+        </li>
+        {/if}
+    </ul>
+</footer>
+
+<style> 
+    footer a {
+        @apply hover:text-zinc-600 dark:hover:text-zinc-300;
+    }
+</style>
