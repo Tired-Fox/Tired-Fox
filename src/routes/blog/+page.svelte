987dba3d5d094dfd0da4b7da958e5aa514fd4cfa
Zachary Boehm <zboehm@menard-inc.com>
2023-12-21 11:11:23 -0600

    [feat:blog] Add basic multi page blog landing

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
-	import type { Post } from '$lib/types';
+    import type { Post } from '$lib/types';
 	import Article from './story/Article.svelte';
-	import Feature from './story/Feature.svelte';
 
 	export let data;
-    function exists(index: number): Boolean {
-        return data.posts.length >= index + 1;
+
+    const pageSize = 4;
+    let numPages = Math.ceil(data.posts.length / pageSize);
+
+    $: page = 0;
+
+    let items: Post[];
+    $: items = data.posts.slice(pageSize*page, Math.min(pageSize*page+pageSize, data.posts.length))
+
+    function next() {
+        if (page < numPages - 1) {
+            page++;
+        }
+        window.scrollTo(0, 0);
     }
-    function post(index: number): Post {
-        return data.posts[index];
+    function previous() {
+        if (page > 0) {
+            page--;
+        }
+        window.scrollTo(0, 0);
     }
 </script>
 
@@ -17,19 +31,40 @@
 </svelete:head>
 
 <section aria-label="Blog posts">
+    <div class="flex justify-between w-full border-y dark:border-zinc-300/70 border-zinc-800/70 mb-8 px-4 py-1 text-sm">
+        <span>Page {page+1}/{numPages}</span>
+        <em>Written by Zachary Boehm</em>
+        <span>Articles {data.posts.length}</span>
+    </div>
 	<ul class="grid justify-between">
-		{#if data.posts.length}
-            {#each data.posts as post}
-                <Article post={post} />
+		{#if items}
+            {#each items as post, i}
+                <Article post={post} featured={i==0&&page==0} />
             {/each}
         {:else}
             <li>No Posts</li>
         {/if}
+        <li class={`flex items-center justify-center gap-4 full mt-12 ${numPages < 2 ? 'hide':''}`}>
+            <button type="button" on:click={previous} disabled={page==0} aria-label="Previous Page">
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="12" viewBox="0 0 256 512" fill="currentColor"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M9.4 278.6c-12.5-12.5-12.5-32.8 0-45.3l128-128c9.2-9.2 22.9-11.9 34.9-6.9s19.8 16.6 19.8 29.6l0 256c0 12.9-7.8 24.6-19.8 29.6s-25.7 2.2-34.9-6.9l-128-128z"/></svg>
+            </button>
+            {page+1} / {numPages}
+            <button type="button" on:click={next} disabled={page==numPages-1} aria-label="Next Page">
+                <svg xmlns="http://www.w3.org/2000/svg" height="20" width="12" viewBox="0 0 256 512" fill="currentColor"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M246.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-128-128c-9.2-9.2-22.9-11.9-34.9-6.9s-19.8 16.6-19.8 29.6l0 256c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l128-128z"/></svg>
+            </button>
+        </li>
 	</ul>
 </section>
 <style>
+    .hide {
+        @apply hidden;
+    }
     ul.grid {
         grid-template-columns: repeat(2, 1fr);
         grid-template-rows: auto;
+    }
+
+    .full {
+        grid-column: 1/-1;
     }
 </style>
--- src/routes/blog/story/Article.svelte
+++ src/routes/blog/story/Article.svelte
@@ -3,14 +3,15 @@
 	import { formatDate } from "$lib/utils";
 
     export let post: Post;
+    export let featured: Boolean;
 </script>
 
-<li class="article">
-    <a href={`/blog/${post.slug}`} class="grid gap-4">
+<li class={`article ${featured?'featured':''}`}>
+    <a href={`/blog/${post.slug}`} class="grid gap-4" aria-label={`Go to article named ${post.title}`}>
+        <h3 class="title text-2xl font-bold mb-3">{post.title}</h3>
         <div class="image w-full h-full overflow-hidden rounded-md shadow-sm shadow-black">  
             <img src={post.hero} alt="Hero" class="object-cover w-full h-full"/>
         </div>
-        <h3 class="title text-2xl font-bold mb-3">{post.title}</h3>
         <article class="content prose dark:prose-invert max-w-none">
             <div class="border-b border-current/50 pb-1">{formatDate(post.updated || post.published)}</div>
             {#if post.preview}
@@ -19,6 +20,7 @@
         </article>
     </a>
 </li>
+
 <style>
     article {
         hyphens: auto;
@@ -27,6 +29,7 @@
         initial-letter: 2;
         padding-right: .5em;
     }
+    
 
     .article a {
         grid-template-columns: repeat(4, auto);
@@ -41,107 +44,128 @@
     .article .image img {
         object-fit: cover;
     }
+
     .article .content {
         --webkit-columns: 1;
         --moz-columns: 1;
         columns: 1;
         column-gap: 3rem;
     }
+
+    .article {
+        grid-column: 1/-1;
+    }
+    .article .image {
+        grid-column: 1/-1;
+    }
+    .article .title {
+        grid-column: 1/-1;
+    }
+    .article .content {
+        grid-column: 1/-1;
+    }
+
+    .article .image {
+        @apply hidden;
+    }
+
+
     @media only screen and (min-width: 786px) {
+        .article .image {
+            @apply block;
+        }
         .article .content {
             --webkit-colulmns: 2;
             --moz-columns: 2;
             columns: 2;
         }
-    }
-
-    .article:first-child  {
-        grid-column: 1/3;
-    }
-    .article:first-child .image {
-        grid-row: 2/4;
-        grid-column: 1/4;
-    }
-    .article:first-child .title {
-        grid-row: 1/2;
-        grid-column: 1/4;
-    }
-    .article:first-child .content {
-        grid-row: 4/5;
-        grid-column: 1/4;
-    }
-
-    @media only screen and (min-width: 786px) {
         .article:nth-child(n+2) .content {
             --webkit-colulmns: 1;
             --moz-columns: 1;
             columns: 1;
         }
-    }
+        .article:nth-child(3n + 2) {
+            @apply border dark:border-zinc-300/70 border-zinc-800/70 pl-8 pt-8;
+            grid-column: 1/2;
+        }
+        .article:nth-child(3n + 2) .image {
+            @apply hidden;
+        }
+        .article:nth-child(3n + 2) .title {
+            grid-row: 1;
+            grid-column: 1/3;
+        }
+        .article:nth-child(3n + 2) .content {
+            grid-row: 2;
+            grid-column: 1/3;
+        }
 
-    .article:nth-child(3n + 3) {
-        @apply border-y border-l dark:border-zinc-300/70 border-zinc-800/70 pl-8;
-    }
-    .article:nth-child(3n + 3) .image {
-        @apply hidden;
-    }
-    .article:nth-child(3n + 3) .title {
-        grid-row: 1;
-        grid-column: 1/3;
-    }
-    .article:nth-child(3n + 3) .content {
-        grid-row: 2;
-        grid-column: 1/3;
-    }
-    .article:nth-child(3n + 4) {
-        @apply border-y border-r dark:border-zinc-300/70 border-zinc-800/70 pl-8;
-    }
-    .article:nth-child(3n + 4) .image {
-        @apply hidden;
-    }
-    .article:nth-child(3n + 4) .title {
-        grid-row: 1;
-        grid-column: 1/3;
-    }
-    .article:nth-child(3n + 4) .content {
-        grid-row: 2;
-        grid-column: 1/3;
-    }
+        .article:nth-child(3n + 3) {
+            @apply border dark:border-zinc-300/70 border-zinc-800/70 pr-8 pt-8;
+            grid-column: 2/-1;
+        }
+        .article:nth-child(3n + 3) .image {
+            @apply hidden;
+        }
+        .article:nth-child(3n + 3) .title {
+            grid-row: 1;
+            grid-column: 3/-1;
+        }
+        .article:nth-child(3n + 3) .content {
+            grid-row: 2;
+            grid-column: 3/-1;
+        }
 
-    
+        
 
-    .article:nth-child(8n + 2) {
-        grid-column: 1/3;
-    }
-    .article:nth-child(8n + 2) .image {
-        grid-row: 1/5;
-        grid-column: 1/3;
-        aspect-ratio: 5/4;
-    }
-    .article:nth-child(8n + 2) .title {
-        grid-row: 1/2;
-        grid-column: 3/5;
-    }
-    .article:nth-child(8n + 2) .content {
-        grid-row: 2/5;
-        grid-column: 3/-1;
-    }
+        .article:nth-child(8n + 1) {
+            grid-column: 1/3;
+        }
+        .article:nth-child(8n + 1) .image {
+            grid-row: 1/5;
+            grid-column: 1/3;
+            aspect-ratio: 5/4;
+        }
+        .article:nth-child(8n + 1) .title {
+            grid-row: 1/2;
+            grid-column: 3/-1;
+        }
+        .article:nth-child(8n + 1) .content {
+            grid-row: 2/5;
+            grid-column: 3/-1;
+        }
 
-    .article:nth-child(8n + 5) {
-        grid-column: 1/3;
-        border-top: none;
-    }
-    .article:nth-child(8n + 5) .image {
-        grid-row: 1/5;
-        grid-column: 3/5;
-        aspect-ratio: 5/4;
-    }
-    .article:nth-child(8n + 5) .title {
-        grid-row: 1/2;
-        grid-column: 1/3;
-    }
-    .article:nth-child(8n + 5) .content {
-        grid-row: 2/5;
-        grid-column: 1/3;
+        .article:nth-child(8n + 4) {
+            grid-column: 1/3;
+            border-top: none;
+        }
+        .article:nth-child(8n + 4) .image {
+            grid-row: 1/5;
+            grid-column: 3/-1;
+            aspect-ratio: 5/4;
+        }
+        .article:nth-child(8n + 4) .title {
+            grid-row: 1/2;
+            grid-column: 1/3;
+        }
+        .article:nth-child(8n + 4) .content {
+            grid-row: 2/5;
+            grid-column: 1/3;
+        }
+        .featured.article  {
+            grid-column: 1/3;
+        }
+        .featured.article .image {
+            grid-row: 1/3;
+            grid-column: 1/-1;
+        }
+        .featured.article .title {
+            grid-row: 3/4;
+            grid-column: 1/-1;
+        }
+        .featured.article .content {
+            grid-row: 4/5;
+            grid-column: 1/-1;
+        }
     }
 </style>
