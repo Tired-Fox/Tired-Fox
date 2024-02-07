bcb1b50d06e0a951261a659554f3d9fa4ce1f89a
Zachary Boehm <zboehm@menard-inc.com>
2024-02-06 18:27:52 -0600

    [fix] Tailwind config and base head metadata

--- astro.config.ts
+++ astro.config.ts
@@ -8,8 +8,10 @@ import spotlight from "@spotlightjs/astro";
 
 import tailwind from "@astrojs/tailwind";
 
+const NEEDS_BASE = false;
+
 export default defineConfig({
-    integrations: [sitemap(), mdx(), sentry(), spotlight(), tailwind({ nesting: true })],
+    integrations: [sitemap(), mdx(), sentry(), spotlight(), tailwind({ nesting: true, configFile: "./tailwind.config.js" })],
     markdown: {
         gfm: true,
         remarkPlugins: [],
@@ -19,5 +21,7 @@ export default defineConfig({
             langs: [],
             wrap: false,
         }
-    }
+    },
+    site: process.env.NODE_ENV === 'production' ? 'https://tired-fox.vercel.app/' : 'https://localhost:4321',
+    base: NEEDS_BASE ? 'Tired-Fox' : ''
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,jdx,ts,tsx"],
+  content: [
+    "./src/**/*.{astro,html,js,jsx,md,mdx,jdx,ts,tsx}"
+  ],
   theme: {
     extend: {},
   },
